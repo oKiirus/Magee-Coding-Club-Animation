82072d612bcb97692f8a334e9734d1455331e483
oKiirus <77072551+oKiirus@users.noreply.github.com>
2024-10-07 08:37:30 -0700

    Add files via upload

---
+++ animation/ball.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+@onready var timer = %spawnGodot
+@onready var trail = %Trail
+var image = preload("res://godot_sprite.tscn")
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	pass
